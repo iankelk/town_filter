5751d96adf50a7dd1ca3539588e967a8789f56ac
Ian Kelk <iankelk@gmail.com>
2012-11-09 14:52:35 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TownFilter::Application.config.secret_token = '57a6624d823c64698e49b184ae73014c7ed4fc0838c8a15e6e0a8477dc02673a17bf417f0919db065c71ad45318baf3d8a04c0421feaa92c7bedff0c020fb3a2'
